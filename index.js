576f7b2f342194e46f76c165ec9b8e8b07ff7a98
deb boro <debajit.boro666@gmail.com>
2022-07-19 01:13:20 -0400

    prompt inquirer set

--- index.js
+++ index.js
@@ -1,99 +1,111 @@
 const inquirer = require('inquirer')
 const db = require('./db/connection')
+const cTable = require('console.table')
 
 //start server after DB connection
 db.connect((err) => {
   if (err) throw err
-  console.log('Database connected')
-  addEmployee()
+  console.log('*******Database connected*********')
+  empDetails()
 })
 
-function addEmployee() {
-  const sql1 = `select * from employee`
-
-  const sql2 = `select * from department`
-
-  db.query(sql1, function (err, res1) {
-    if (err) throw err
-    db.query(sql2, function (err, res2) {
-      if (err) throw err
-
-      inquirer
-        .prompt([
-          {
-            type: 'input',
-            name: 'first_name',
-            message: "What is the employee's first name?",
-          },
-          {
-            type: 'input',
-            name: 'last_name',
-            message: "What is the employee's last name?",
-          },
-          {
-            type: 'rawlist',
-            name: 'role_id',
-            message: "What is the employee's position?",
-            choices: function () {
-              let rolesArr = []
-              for (let i = 0; i < res1.length; i++) {
-                rolesArr.push({ name: res1[i].Position, value: res1[i].id })
-              }
-              return rolesArr
-            },
-          },
-          {
-            type: 'rawlist',
-            name: 'manager_id',
-            message: "Who is this employee's manager?",
-            choices: function () {
-              let managerArr = []
-              for (let i = 0; i < res2.length; i++) {
-                managerArr.push({ name: res2[i].Manager, value: res2[i].id })
-              }
-              return managerArr
-            },
-          },
-        ])
-        // .then((answer) => {
-        //   db.query(
-        //     `INSERT INTO employee (first_name, last_name, role_id , manager_id) VALUES (?,?,?,?)`,
-        //     [
-        //       answer.first_name,
-        //       answer.last_name,
-        //       answer.role_id,
-        //       answer.manager_id,
-        //     ],
-        //     (err, res) => {
-        //       console.table(res, answer)
-        //     },
-        //   )
-        // })
-        .then((data) => {
-          const sql = `SELECT department.id, name AS "Department" FROM department`
-          db.query(sql, function (err, res) {
-            if (err) throw err
-
-            inquirer
-              .prompt([
-                {
-                  name: 'name',
-                  type: 'input',
-                  message: 'What department would you like to add?',
-                },
-              ])
-              .then((answer) => {
-                db.query(
-                  `INSERT INTO departments (name) VALUE (?)`,
-                  [answer.name],
-                  (err, answer) => {
-                    console.table(res, answer)
-                    return answer
-                  },
-                )
-              })
+const empDetails = () => {
+  return inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'option',
+        message: 'What would you like to do?',
+        choices: [
+          'View all departments',
+          'View all roles',
+          'View all employees',
+          'Add a department',
+          'Add a role',
+          'Add an employee',
+          'Update an employee role',
+          'Quit',
+        ],
+      },
+    ])
+    .then((answer) => {
+      switch (answer.option) {
+        case 'View all departments':
+          const sql1 = 'SELECT * FROM department'
+          db.query(sql1, (error, result) => {
+            if (error) throw error
+            console.table(result)
+            empDetails()
           })
-        })
+          break
+        case 'View all roles':
+          const sql2 =
+            'select roles.id as id , roles.title as role, roles.salary, department.name as department from roles left join department on roles.department_id=department.id'
+          db.query(sql2, (error, result) => {
+            if (error) throw error
+            console.table(result)
+            empDetails()
+          })
+          break
+        case 'View all employees':
+          const sql3 =
+            'Select EMP.id, EMP.first_name, EMP.last_name,EMP.role, employee.first_name as manager from (select emp.id, emp.first_name, emp.last_name, roles.title as role, emp.manager_id from employee emp left join roles roles on emp.role_id=roles.id ) as EMP left join employee on EMP.manager_id = employee.id'
+          db.query(sql3, (error, result) => {
+            if (error) throw error
+            console.table(result)
+            empDetails()
+          })
+          break
+
+        case 'Add a department':
+          inquirer
+            .prompt([
+              {
+                type: 'input',
+                name: 'department',
+                message: 'What is the name of the department?',
+              },
+            ])
+            .then((answer) => {
+              const sqlInsert = 'INSERT INTO department (name) VALUES (?)'
+              db.query(sqlInsert, [answer.department], (error, result) => {
+                if (error) throw error
+                empDetails()
+              })
+            })
+          break
+
+        case 'Add a role':
+          inquirer
+            .prompt([
+              {
+                type: 'input',
+                name: 'role',
+                message: 'What is the role you want to add?',
+              },
+              {
+                type: 'input',
+                name: 'salary',
+                message: 'What is the salary for the role?',
+              },
+              {
+                type: 'input',
+                name: 'department',
+                message: 'Which department does the role belong to?',
+              },
+            ])
+            .then((answer) => {
+              const sqlSelect = 'SELECT id FROM department where name= ?'
+              db.query(sqlSelect, [answer.department], (error, result) => {
+                if (error) throw error
+                console.log(result)
+              })
+              empDetails()
+            })
+
+          break
+        case 'Quit':
+          break
+      }
     })
-  })
 }
