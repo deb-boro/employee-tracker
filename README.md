4139e6e36d2217b2f9c3799b5e22f8e444b5e8e7
deb boro <debajit.boro666@gmail.com>
2022-07-26 21:43:11 -0400

    updated README file

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # employee-tracker
+
+The employee-tracker is a node js based application to add, update, delete and retrieve employee data for the organization. The application uses inquirer prompt to capture user input and mysql database to store employee data. There are different mysql tables available to store data e.g. department, employee, roles databases.
+
+## Authors
+
+[@debajitboro](https://www.github.com/deb-boro)
+
+## Walkthrough Video
+
+[![Video]](https://user-images.githubusercontent.com/100662344/181141093-6c830f31-05be-4f65-b5e9-518b7ecff59e.mp4)
+
+## Features
+
+Employee-Tracker is built using - node js, inquirer, mysql2.
+
+## Tech Stack
+
+javascript, node.js, inquirer package, mysql2 package, console.table package
+
+## Link to Deployment
+
+Copy and paste below link in any browser:
+https://github.com/deb-boro/employee-tracker
+
+## Screenshot
+
+![EmployeeTracker](https://user-images.githubusercontent.com/100662344/181142108-28eba81f-5f32-444d-b6be-542d411d2b34.png)
--- index.js
+++ index.js
@@ -8,8 +8,18 @@ db.connect((err) => {
   console.log('*******Database connected*********')
   empDetails()
 })
+async function deptChoices() {
+  db.query('SELECT * FROM department', (error, result) => {
+    if (error) throw error
+    let departments = [result]
+    const departmentChoices = departments.map(({ id, named }) => ({
+      name: named,
+      value: id,
+    }))
+  })
+}
 
-const empDetails = () => {
+const empDetails = async () => {
   return inquirer
     .prompt([
       {
@@ -49,7 +59,7 @@ const empDetails = () => {
           break
         case 'View all employees':
           const sql3 =
-            'Select EMP.id, EMP.first_name, EMP.last_name,EMP.role, employee.first_name as manager from (select emp.id, emp.first_name, emp.last_name, roles.title as role, emp.manager_id from employee emp left join roles roles on emp.role_id=roles.id ) as EMP left join employee on EMP.manager_id = employee.id'
+            'Select EMP.id, EMP.first_name, EMP.last_name,EMP.role, CONCAT(employee.first_name, " ", employee.last_name) as Manager from (select emp.id, emp.first_name, emp.last_name, roles.title as role, emp.manager_id from employee emp left join roles roles on emp.role_id=roles.id ) as EMP left join employee on EMP.manager_id = employee.id'
           db.query(sql3, (error, result) => {
             if (error) throw error
             console.table(result)
@@ -95,18 +105,123 @@ const empDetails = () => {
                 type: 'input',
                 name: 'department',
                 message: 'Which department does the role belong to?',
+                choices: deptChoices(),
               },
             ])
             .then((answer) => {
               const sqlSelect = 'SELECT id FROM department where name= ?'
               db.query(sqlSelect, [answer.department], (error, result) => {
                 if (error) throw error
-                console.log(result)
-              })
-              empDetails()
-            })
+                const rolesID = result[0].id
+                const role = answer.role
+                const salary = answer.salary
+                const sqlInsert =
+                  'Insert into roles (title, salary, department_id) values (?, ?, ?)'
+                db.query(
+                  sqlInsert,
+                  [role, salary, rolesID],
+                  (error, result) => {
+                    if (error) throw error
+                  },
+                )
 
+                empDetails()
+              })
+            })
           break
+        case 'Add an employee':
+          inquirer
+            .prompt([
+              {
+                type: 'input',
+                name: 'first_name',
+                message: 'What is the first name? ',
+              },
+              {
+                type: 'input',
+                name: 'last_name',
+                message: 'What is the last name? ',
+              },
+              {
+                type: 'input',
+                name: 'role',
+                message: 'What is the role? ',
+              },
+              {
+                type: 'input',
+                name: 'manager',
+                message: 'Enter manager name? ',
+              },
+            ])
+            .then((answer) => {
+              const sqlSelect = 'SELECT id FROM roles where title = ?'
+              db.query(sqlSelect, [answer.role], (error, result) => {
+                if (error) throw error
+                const rolesID = result[0].id
+                const first_name = answer.first_name
+                const last_name = answer.last_name
+                const manager_name = answer.manager
+                const [
+                  firstName_Manager,
+                  lastName_Manager,
+                ] = manager_name.split(' ')
+
+                const sqlSelect2 =
+                  'select id from employee where first_name = ? and last_name= ?'
+                db.query(
+                  sqlSelect2,
+                  [firstName_Manager, lastName_Manager],
+                  (error, result) => {
+                    if (error) throw error
+                    const manager_id = result[0].id
+                    const sqlInsert =
+                      'insert into employee (first_name, last_name,role_id,manager_id) values (?,?,?,?)'
+                    db.query(
+                      sqlInsert,
+                      [first_name, last_name, rolesID, manager_id],
+                      (error, result) => {
+                        if (error) throw error
+                      },
+                    )
+                  },
+                )
+
+                empDetails()
+              })
+            })
+          break
+        case 'Update an employee role':
+          inquirer
+            .prompt([
+              {
+                type: 'input',
+                name: 'employee_name',
+                message: 'What is the name of the employee?',
+              },
+              {
+                type: 'input',
+                name: 'role',
+                message: 'What is the new role?',
+              },
+            ])
+            .then((answer) => {
+              const [first_name, last_name] = answer.employee_name.split(' ')
+
+              const sqlSelect4 =
+                'select role_id from employee where first_name=? and last_name=?'
+
+              db.query(sqlSelect4, [first_name, last_name], (error, result) => {
+                const role_id = result[0].role_id
+                const sqlUpdate = 'update roles set title=? where id=?'
+
+                db.query(sqlUpdate, [answer.role, role_id], (error, result) => {
+                  if (error) throw error
+                  empDetails()
+                })
+              })
+            })
+          break
+
         case 'Quit':
           console.log('.......... Bye for now............')
           db.end()
